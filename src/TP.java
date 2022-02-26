15d2a4dbc8ee022e54ea1d71a556d6cfa81443e9
Christos Cunning <christoscunn@gmail.com>
2022-02-26 16:02:10 -0500

    output data to json file, update gson 2.2.2->2.9.0

--- src/TP.java
+++ src/TP.java
@@ -1,12 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 
 public class TP {
@@ -68,6 +78,52 @@ public class TP {
 		
 		System.out.println(loads.get(0));
 		System.out.println(trips.get(0));
+		
+		
+		// Begin calculations
+		for (TripPlanRequest trip : trips) {
+			// find most profitable route for trip
+			
+			
+			trip.load_ids.add(loads.get(0).getLoadId());
+		}
+		
+		
+		
+		// Output to json
+		String json_output = "";
+		List<Map<String, Object>> output_data = new ArrayList<Map<String, Object>>();
+		for (TripPlanRequest trip : trips) {
+			Map<String, Object> trip_output_map = new LinkedHashMap<String, Object>();
+			trip_output_map.put("input_trip_id", trip.getInputTripId());
+			trip_output_map.put("loads_ids", trip.load_ids);
+			output_data.add(trip_output_map);
+		}
+		StringWriter out = new StringWriter();
+		try {
+			JSONValue.writeJSONString(output_data, out);
+			json_output = out.toString();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// pretify json
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		JsonElement je = JsonParser.parseString(json_output);
+		String pretty_json_output = gson.toJson(je);
+		// write json to file
+		//System.out.println(json_output);
+		try {
+			String outputFilePath = new File("output").getAbsoluteFile().toString() + "\\trip_plan_output.json";
+			PrintWriter printOut = new PrintWriter(outputFilePath);
+			printOut.print(pretty_json_output);
+			printOut.close();
+			System.out.println("Trip plan results outputted to " + outputFilePath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
 
